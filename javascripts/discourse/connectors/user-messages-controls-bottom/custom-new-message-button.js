f826f208490a9b017428044d7b0bfda0bce05f47
awesomerobot <kris.aubuchon@discourse.org>
2023-09-01 15:15:16 -0400

    UX: add custom New Message button to /my/messages

---
+++ javascripts/discourse/connectors/user-messages-controls-bottom/custom-new-message-button.js
@@ -0,0 +1,20 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import Composer from "discourse/models/composer";
+
+export default class CustomNewMessageButton extends Component {
+  @service composer;
+  showNewPM = this.args.outletArgs.showNewPM;
+
+  @action
+  customCreateNewMessage() {
+    const recipients = settings.pm_recipients;
+    this.composer.open({
+      action: Composer.PRIVATE_MESSAGE,
+      recipients,
+      archetypeId: "private_message",
+      draftKey: Composer.NEW_PRIVATE_MESSAGE_KEY,
+    });
+  }
+}
